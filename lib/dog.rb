e2fef8f812bb280dd315460eb55f3a4c282dc137
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-23 18:51:59 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,32 @@
+class Dog 
+  def initialize(name,breed)
+    @name = name 
+    @breed = breed 
+  end 
+  
+  def name=(name)
+    @name 
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def breed=(breed) 
+   if @breed 
+     @breed 
+   else
+     "Mutt"
+   end
+  end 
+  
+  def breed  
+   if @breed 
+     @breed 
+   else
+     "Mutt"
+   end
+  end 
+end 
+
+
